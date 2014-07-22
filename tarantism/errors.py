160b8f8705e92de545338b5ffea575313d4df0cf
Sergey Orlov <sergey.orlov@corp.mail.ru>
2014-07-22 12:36:38 +0400

    Exceptions.

--- tarantism/errors.py
+++ tarantism/errors.py
@@ -2,3 +2,15 @@
 
 class DoesNotExist(Exception):
     pass
+
+
+class MultipleObjectsReturned(Exception):
+    pass
+
+
+class ImproperlyConfigured(Exception):
+    pass
+
+
+class ValidationError(Exception):
+    pass
