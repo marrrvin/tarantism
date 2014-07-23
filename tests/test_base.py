536977eb4f0e5e2fef8d711aa06ef1e973d94ac2
Sergey Orlov <sergey.orlov@corp.mail.ru>
2014-07-23 12:14:01 +0400

    unittests.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,50 +1,45 @@
 
-import unittest
+from unittest import TestCase
 
-from tarantism import DEFAULT_ALIAS
-from tarantism import register_connection
-from tarantism import get_space
 from tarantism import Model
 from tarantism import LongField
 from tarantism import StringField
 
 
-class Record(Model):
-    pk = LongField(
-        min_value=0
-    )
-    data = StringField(
-        min_length=1,
-        max_length=100
-    )
+class CreateModelTestCase(TestCase):
+    def test_default(self):
+        class Record(Model):
+            pk = LongField()
+            data = StringField(default='test')
 
+        r = Record()
 
-class BaseTestCase(unittest.TestCase):
-    def setUp(self):
-        self.tnt_config = {
-            'host': '127.0.0.1',
-            'port': 33013,
-            'space': 0
-        }
-
-        register_connection(DEFAULT_ALIAS, **self.tnt_config)
-        space = get_space()
-        space.connection.call(
-            'clear_space', (str(self.tnt_config['space']),)
-        )
+        self.assertEqual('test', r.data)
 
     def test_create(self):
+        class Record(Model):
+            pk = LongField()
+            data = StringField()
+
         pk = 1L
+        data = 'test'
 
-        record = Record(
-            pk=pk,
-            data='test'
-        )
-        actual_record = record.save()
+        r = Record(pk=pk, data=data)
 
-        self.assertEqual(record.pk, actual_record.pk)
-        self.assertEqual(record.data, actual_record.data)
+        self.assertEqual(pk, r.pk)
+        self.assertEqual(data, r.data)
 
-        record.delete()
+    def test_setters(self):
+        class Record(Model):
+            pk = LongField()
+            data = StringField()
 
-        self.assertRaises(Record.DoesNotExist, Record.objects.get, pk)
+        pk = 1L
+        data = 'test'
+
+        r = Record()
+        r.pk = pk
+        r.data = data
+
+        self.assertEqual(pk, r.pk)
+        self.assertEqual(data, r.data)
