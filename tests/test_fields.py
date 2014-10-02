911ecb38b26455cb1e3f97b8f8cd89b7e1d6679c
Sergey Orlov <sergey.orlov@corp.mail.ru>
2014-10-02 19:02:18 +0400

    Fields tests.

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -2,66 +2,113 @@
 from unittest import TestCase
 
 from tarantism import Model
+from tarantism import BaseField
+from tarantism import IntField
 from tarantism import LongField
 from tarantism import StringField
 from tarantism import ValidationError
 
 
-class ValidationTestCase(TestCase):
+class BaseFieldTestCase(TestCase):
+    def test_simple(self):
+        field = BaseField()
+        value = u'test'
+
+        self.assertEqual(value, field.to_python(value))
+
+
+class FieldRequiredValidationTestCase(TestCase):
     def test_fail_on_required_field(self):
         class Record(Model):
             pk = LongField()
-            data = StringField()
 
         r = Record()
 
         self.assertRaises(ValidationError, r.validate)
 
-    def test_fail_on_field_too_long(self):
-        class Record(Model):
-            pk = LongField()
-            data = StringField(
-                min_length=1, max_length=1
-            )
 
-        r = Record(pk=1L, data='test')
+class IntFieldValidationTestCase(TestCase):
+    def test_fail_on_invalid_type(self):
+        class Record(Model):
+            pk = IntField(min_value=1)
+
+        r = Record(pk='invalid-pk')
+        self.assertRaises(ValidationError, r.validate)
+
+    def test_fail_on_min_value(self):
+        class Record(Model):
+            pk = IntField(min_value=1)
+
+        r = Record(pk=-1L)
 
         self.assertRaises(ValidationError, r.validate)
 
-    def test_fail_on_field_value_too_small(self):
+    def test_fail_on_max_value(self):
+        class Record(Model):
+            pk = IntField(max_value=1)
+
+        r = Record(pk=2L)
+
+        self.assertRaises(ValidationError, r.validate)
+
+
+class LongFieldValidationTestCase(TestCase):
+    def test_fail_on_invalid_type(self):
         class Record(Model):
             pk = LongField(min_value=1)
-            data = StringField()
 
-        r = Record(pk=-1L, data='test')
+        r = Record(pk='invalid-pk')
+        self.assertRaises(ValidationError, r.validate)
+
+    def test_fail_on_min_value(self):
+        class Record(Model):
+            pk = LongField(min_value=1)
+
+        r = Record(pk=-1L)
 
         self.assertRaises(ValidationError, r.validate)
 
+    def test_fail_on_max_value(self):
+        class Record(Model):
+            pk = LongField(max_value=1)
+
+        r = Record(pk=2L)
+
+        self.assertRaises(ValidationError, r.validate)
+
+
+class StringFieldValidationTestCase(TestCase):
+    def test_fail_on_min_length(self):
+        class Record(Model):
+            data = StringField(min_length=1)
+
+        r = Record(data=u'')
+
+        self.assertRaises(ValidationError, r.validate)
+
+    def test_fail_on_max_length(self):
+        class Record(Model):
+            data = StringField(max_length=1)
+
+        r = Record(data=u'test')
+
+        self.assertRaises(ValidationError, r.validate)
+
+    def test_fail_on_regex(self):
+        class Record(Model):
+            data = StringField(regex=u'^\d+$')
+
+        r = Record(data=u'123test45')
+
+        self.assertRaises(ValidationError, r.validate)
+
+
+class ValidationPassedTestCase(TestCase):
     def test_pass(self):
         class Record(Model):
-            pk = LongField(
-                min_value=0
-            )
-            data = StringField(
-                min_length=1,
-                max_length=100
-            )
+            pk = LongField(min_value=0)
+            data = StringField(min_length=1, max_length=100)
 
         r = Record(pk=1L, data='test')
 
         self.assertEqual(None, r.validate())
-
-
-class DefaultsTestCase(TestCase):
-    def test_defaults(self):
-        default_pk = 1L
-        default_data = u'Test data'
-
-        class Record(Model):
-            pk = LongField(default=default_pk)
-            data = StringField(default=default_data)
-
-        r = Record()
-
-        self.assertEqual(default_pk, r.pk)
-        self.assertEqual(default_data, r.data)
