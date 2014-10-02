31e080f585ef81b51bbaed4be35d901786dd6b25
Sergey Orlov <sergey.orlov@corp.mail.ru>
2014-10-02 15:50:11 +0400

    connection module refactoring.

--- tarantism/connection.py
+++ tarantism/connection.py
@@ -1,62 +1,68 @@
 
 import tarantool
+from tarantool import DatabaseError
 
 
 __all__ = [
-    'DEFAULT_ALIAS', 'DEFAULT_SETTINGS',
-    'register_connection', 'get_connection',
-    'disconnect', 'get_space', 'connect'
+    'DEFAULT_ALIAS', 'DEFAULT_HOST', 'DEFAULT_PORT', 'DEFAULT_SPACE',
+    'ConnectionError',
+    'connect', 'disconnect',
+    'register_connection', 'get_connection', 'get_space',
 ]
 
 
 _connection_settings = {}
-_connections = {}
-_spaces = {}
+"""Aliases to settings mapping."""
 
+_connections = {}
+"""Aliases to Tarantool connection objects mapping."""
+
+_spaces = {}
+"""Aliases to Tarantool space objects mapping."""
 
 DEFAULT_ALIAS = 'default'
 
-DEFAULT_SETTINGS = {
-    'host': 'localhost',
-    'port': 33013,
-    'space': 0
-}
+DEFAULT_HOST = 'localhost'
+
+DEFAULT_PORT = 33013
+
+DEFAULT_SPACE = 0
 
 
-def register_connection(alias, **kwargs):
+class ConnectionError(Exception):
+    """Wraps tarantool driver connection errors."""
+    pass
+
+
+def register_connection(alias, host=None, port=None, space=None, **kwargs):
+    """Register connection settings for alias.
+
+    :param alias:
+    :param host:
+    :param port:
+    :param space:
+
+    """
     global _connection_settings
 
-    conn_settings = DEFAULT_SETTINGS.copy()
+    conn_settings = {
+        'host': host or DEFAULT_HOST,
+        'port': port or DEFAULT_PORT,
+        'space': space or DEFAULT_SPACE
+    }
     conn_settings.update(kwargs)
 
     _connection_settings[alias] = conn_settings
 
-
-def get_connection(alias=DEFAULT_ALIAS, reconnect=False):
-    global _connections
-
-    if reconnect:
-        disconnect(alias)
-
-    if alias not in _connections:
-        conn_settings = _connection_settings.get(alias)
-
-        if not _connection_settings:
-            raise ValueError(
-                'Connection with alias {alias} have not defined.'.format(
-                    alias=alias
-                )
-            )
-
-        _connections[alias] = tarantool.connect(
-            host=conn_settings.get('host'),
-            port=conn_settings.get('port')
-        )
-
-    return _connections[alias]
+    return _connection_settings[alias]
 
 
 def disconnect(alias=DEFAULT_ALIAS):
+    """Close connection by alias.
+
+    :param alias:
+
+    """
     global _connections
     global _spaces
 
@@ -66,6 +72,42 @@ def disconnect(alias=DEFAULT_ALIAS):
 
     if alias in _spaces:
         del _spaces[alias]
+
+
+def get_connection(alias=DEFAULT_ALIAS, reconnect=False):
+    """Return connection by alias.
+
+    :param alias:
+    :param reconnect:
+
+    """
+    global _connections
+
+    if reconnect:
+        disconnect(alias)
+
+    if alias not in _connections:
+        alias_settings = _connection_settings.get(alias)
+
+        if not alias_settings:
+            raise ValueError(
+                'Connection with alias {alias} have not defined.'.format(
+                    alias=alias
+                )
+            )
+
+        try:
+            _connections[alias] = tarantool.connect(
+                host=alias_settings.get('host'),
+                port=alias_settings.get('port')
+            )
+        except DatabaseError as exc:
+            raise ConnectionError(
+                'Connect error for alias "{alias}": "{message}".'.format(
+                    alias=alias, message=exc
+                ))
+
+    return _connections[alias]
 
 
 def get_space(alias=DEFAULT_ALIAS, reconnect=False):
@@ -83,10 +125,10 @@ def get_space(alias=DEFAULT_ALIAS, reconnect=False):
     return _spaces[alias]
 
 
-def connect(space, alias=DEFAULT_ALIAS, **kwargs):
+def connect(alias=DEFAULT_ALIAS, **kwargs):
     global _connections
 
     if alias not in _connections:
-        register_connection(alias, space, **kwargs)
+        register_connection(alias, **kwargs)
 
-    return get_connection(alias, **kwargs)
+    return get_connection(alias)
