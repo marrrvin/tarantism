4313eb4cc464dfa0bd8125c6be399e0f7b3613df
Sergey Orlov <sergey.orlov@corp.mail.ru>
2014-10-14 18:07:30 +0400

    Json optional args passed to JsonField.

--- tarantism/contrib/fields.py
+++ tarantism/contrib/fields.py
@@ -5,8 +5,14 @@ from tarantism.fields import BaseField
 
 
 class JsonField(BaseField):
+    def __init__(self, *args, **kwargs):
+        self.dump_kwargs = kwargs.get('dump_kwargs', {})
+        self.load_kwargs = kwargs.get('load_kwargs', {})
+
+        super(JsonField, self).__init__(*args, **kwargs)
+
     def to_db(self, value):
-        return json.dumps(value)
+        return json.dumps(value, **self.dump_kwargs)
 
     def to_python(self, value):
-        return json.loads(value)
+        return json.loads(value, **self.load_kwargs)
